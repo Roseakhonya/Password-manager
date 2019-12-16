3880b18c7c6361c0edf65a237af1a01a26bb5a82
Roseakhonya <akhonyarose@gmail.com>
2019-12-16 15:57:25 +0300

    add credentials list

--- password.py
+++ password.py
@@ -1,6 +1,5 @@
 import random
 import string
-
 class User:
     '''
     This creates user class which generates new instances of a user
@@ -8,11 +7,11 @@ class User:
     user_list = []
 
     def _init_(self, username, password):
-      '''
-      function that define the properties for user objects
-      '''
-      self.username = username
-      self.password = password
+        '''
+        function that define the properties for user objects
+        '''
+        self.username = username
+        self.password = password
 
     def save_user(self):
         '''
@@ -26,17 +25,20 @@ class User:
         '''
         A method that displays username
         '''
+
     def delete_user(self):
         User.user_list.remove(self)
         '''
         A method that deletes a saved username
         '''
 
-    class Credentials():
-        '''
-        Create a credentials class to create new objects of credentials
-        '''
-        credentials_list = []
+
+class Credentials():
+    '''
+    Create a credentials class to create new objects of credentials
+    '''
+    credentials_list = []
+
     def __init__(self, account, userName, password):
         '''
         A function to define the user credentials"
@@ -47,7 +49,7 @@ class User:
 
     def save_details(self):
         '''
-        A function that saves a new credential property to the credentials list
+        method to save a new credential instance to the credentials list
         '''
         Credentials.credentials_list.append(self)
 
@@ -55,7 +57,7 @@ class User:
         '''
         A function which deletes a credential from the existing one
         '''
-    
+
     @classmethod
     def find_credential(cls, account):
         '''
@@ -64,16 +66,27 @@ class User:
         for credential in cls.credentials_list:
             if credential.account == account:
                 return credential
+
     @classmethod
-    def if_credential_exist(cls,account):
+    def if_credential_exist(cls, account):
         '''
-        a function to check if the credential entered exists on the list and it returns true or false
+        function which takes in account name and returns a credential which corresponds to the account
         '''
-        
-    
-    
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
 
+    @classmethod
+    def display_credentials(cls):
+        '''
+        Function that returns all items on the credentials list
+        '''
+        return cls.credentials_list
 
-
-
-     
+    def generatesPassword(self):
+        '''
+        function which generates passwords as string
+        '''
+        password = string.ascii_letters + string.ascii_lowercase + string.digits
+        return ''.join(random.choice(password)for i in range(self))
