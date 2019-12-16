a306fb986df1293b6ca19525efcf568692bd24ec
Roseakhonya <akhonyarose@gmail.com>
2019-12-17 00:08:27 +0300

    import unit test

---
+++ password_test.py
@@ -0,0 +1,10 @@
+import unittest
+from password import User
+from password import Credentials
+
+class TestUser(unittest.TestCase):
+    '''
+    A test class which defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
