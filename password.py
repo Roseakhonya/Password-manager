dbd997c574abeb97f55601ae9c3261e428bcd976
Roseakhonya <akhonyarose@gmail.com>
2019-12-16 10:14:35 +0300

    create class user

---
+++ password.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+class User:
+    '''
+    This creates user class which generates new instances of a user
+    '''
+    user_list = []
+
+    def _init_(self, username, password):
+      '''
+      function that define the properties for user objects
+      '''
+      self.username = username
+      self.password = password
+
+    def save_user(self):
+        '''
+        A function that saves a  new user instance into the user list
+        '''
+        User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls)
+    
+
+
+     
